f1f96d05d7e45117429d6e81030b14b8a371387e
frederic <flausson@normandiewebschool.fr>
2025-01-30 12:27:01 +0100

    Add ApplicationDbContext and update User model namespace

---
+++ PasswordManager.Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PasswordManager.Core.Models;
+
+namespace PasswordManager.Core.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+} 
