e29f1a9b41d8141bf738143ff31a77fe66dc2c82
frederic <flausson@normandiewebschool.fr>
2025-02-21 13:19:55 +0100

    Add validation for password generator length with data annotations

--- PasswordManager.Core/Models/PasswordGeneratorOptions.cs
+++ PasswordManager.Core/Models/PasswordGeneratorOptions.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PasswordManager.Core.Models
 {
     public class PasswordGeneratorOptions
     {
-        public int Length { get; set; } = 12;
+        [Range(8, 32, ErrorMessage = "Password length must be between 8 and 32 characters")]
+        public int Length { get; set; } = 16;
+        
         public bool IncludeUppercase { get; set; } = true;
         public bool IncludeLowercase { get; set; } = true;
         public bool IncludeNumbers { get; set; } = true;
--- PasswordManager.Web/PasswordManager.Web.Client/Models/GeneratePasswordRequest.cs
+++ PasswordManager.Web/PasswordManager.Web.Client/Models/GeneratePasswordRequest.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PasswordManager.Web.Client.Models
 {
     public class GeneratePasswordRequest
     {
+        [Range(8, 32, ErrorMessage = "Password length must be between 8 and 32 characters")]
         public int Length { get; set; } = 16; // Valeur par défaut
         public bool IncludeUppercase { get; set; } = true;
         public bool IncludeLowercase { get; set; } = true;
